6cecc7404861b814bd41a4ef66ff34b6c1c74601
asorsz <asorsz@gmail.com>
2021-03-28 14:07:25 +0100

    Complete Controls layout

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,16 +1,40 @@
 import '../css/Controls.css';
+import { AiFillPlusCircle, AiFillMinusCircle, AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai';
+import { BiReset } from 'react-icons/bi'
 
 const Controls = () => {
   return (
-    <div className="timer__controls">
-        <button data-time="20" className="timer__button">20 Secs</button>
-        <button data-time="300" className="timer__button">Work 5</button>
-        <button data-time="900" className="timer__button">Quick 15</button>
-        <button data-time="1200" className="timer__button">Snack 20</button>
-        <button data-time="3600" className="timer__button">Lunch Break</button>
-        <form name="customForm" id="custom">
-          <input type="text" name="minutes" placeholder="Enter Minutes" />
-        </form>
+    <div id="control-banner">
+      <div className="control-section">
+        <div id="break-label">Break Length</div>
+        <div className="controls">
+          <button id="break-decrement" className="plus-minus"><AiFillMinusCircle /></button>
+          <div id="break-length">5</div>
+          <button id="break-increment" className="plus-minus"><AiFillPlusCircle /></button>
+        </div> 
+      </div>
+      <div className="control-section">
+        <div className="controls">
+          <button id="start_stop" className="play-pause-reset">
+            <AiFillPlayCircle />
+            <AiFillPauseCircle />
+          </button>
+          <button id="reset" className="play-pause-reset"><BiReset /></button>
+        </div>
+      </div>
+      <div className="control-section">
+      <div id="session-label">Session Length</div>
+        <div className="controls">
+          <button id="session-decrement" className="plus-minus"><AiFillMinusCircle /></button>
+          <div id="session-length">25</div>
+          <button id="session-increment" className="plus-minus"><AiFillPlusCircle /></button>
+        </div>
+      </div>
+      {/* <button data-time="20" classNameName="timer__button">20 Secs</button>
+      <button data-time="300" classNameName="timer__button">Work 5</button>
+      <button data-time="900" classNameName="timer__button">Quick 15</button>
+      <button data-time="1200" classNameName="timer__button">Snack 20</button>
+      <button data-time="3600" classNameName="timer__button">Lunch Break</button> */}
     </div>
   )
 };
