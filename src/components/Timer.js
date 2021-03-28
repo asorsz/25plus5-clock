e10df9a7f5638f28b9aee5cd9b9c41da67fc33af
asorsz <asorsz@gmail.com>
2021-03-28 19:55:32 +0100

    Add timer functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,61 +1,93 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import '../css/Timer.css';
 import Controls from './Controls';
 import Display from './Display';
 
 const Timer = () => {
-  const getDisplay = (secs) => {
-    const mins = Math.floor(secs / 60);
-    const rem = secs % 60;
-    const remPad = rem <= 9 ? `0${rem}` : rem;
-    const display = `${mins}:${remPad}`;
-    return display;
-  };
-
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [timer, setTimer] = useState({
-    type: 'Session',
-    seconds: sessionLength * 60,
-    display: getDisplay(sessionLength * 60),
-    playing: false,
-  });
+  const [timerType, setTimerType] = useState('Session');
+  const [seconds, setSeconds] = useState(sessionLength * 60);
+  const [playing, setPlaying] = useState(false);
+  const [then, setThen] = useState(null);
+  const interval = useRef(null);
 
   const reset = () => {
     setBreakLength(5);
     setSessionLength(25);
-    setTimer({
-      type: 'Session',
-      seconds: sessionLength * 60,
-      display: getDisplay(sessionLength * 60),
-      playing: false,
-    })
+    setTimerType('Session');
+    setSeconds(1500);
+    setPlaying(false);
+    setThen(null);
+    const beep = document.getElementById('beep');
+    beep.pause();
+    beep.currentTime = 0;
   };
     
   const handlePlusMinusClick = (e) => {
-    if (timer.playing) return;
+    if (playing) return;
     if (e.target.id === 'break-decrement') setBreakLength(
       breakLength > 1 ? breakLength - 1 : 1
     );
-    if (e.target.id === 'break-increment') setBreakLength(breakLength + 1);
+    if (e.target.id === 'break-increment') setBreakLength(
+      breakLength < 60 ? breakLength + 1 : 60
+    );
     if (e.target.id === 'session-decrement') setSessionLength(
       sessionLength > 1 ? sessionLength - 1 : 1
     );
-    if (e.target.id === 'session-increment') setSessionLength(sessionLength + 1);
+    if (e.target.id === 'session-increment') setSessionLength(
+      sessionLength < 60 ? sessionLength + 1 : 60
+    );
+  };
+
+  const startStop = () => {
+    setPlaying(!playing);
+  };
+
+  const playBeep = () => {
+    const beep = document.getElementById('beep');
+    beep.currentTime = 0;
+    beep.play();  
   };
 
   useEffect(() => {
-    if (!timer.playing) setTimer({
-      ...timer,
-      seconds: sessionLength * 60,
-      display: getDisplay(sessionLength * 60),
-    })
-  }, [sessionLength])
+    if (playing) {
+      setThen(Date.now() + (seconds * 1000));
+    } else {
+      setThen(null)
+    }
+  }, [playing])
+
+  useEffect(() => {
+    clearInterval(interval.current);
+    if (then) interval.current = setInterval(() => updateTimer(), 1000);
+  }, [then])
+
+  const updateTimer = () => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft === 0) playBeep();
+
+    if (secondsLeft < 0 && timerType === 'Session') {
+      setTimerType('Break');
+      setSeconds(breakLength * 60);
+      setThen(Date.now() + (breakLength * 60 * 1000)); 
+    } else if (secondsLeft < 0 && timerType === 'Break') {
+      setTimerType('Session');
+      setSeconds(sessionLength * 60);
+      setThen(Date.now() + (sessionLength * 60 * 1000)); 
+    } else {
+      setSeconds(secondsLeft);
+    }
+  };
+
+  useEffect(() => {
+    if (!playing) setSeconds(sessionLength * 60);
+  }, [sessionLength]);
 
   return (
     <div className="timer">
-      <Controls playing={timer.playing} breakLength={breakLength} sessionLength={sessionLength} handlePlusMinusClick={handlePlusMinusClick} reset={reset}/>
-      <Display label={timer.type} timeLeft={timer.display}/>
+      <Controls playing={playing} breakLength={breakLength} sessionLength={sessionLength} handlePlusMinusClick={handlePlusMinusClick} startStop={startStop} reset={reset}/>
+      <Display label={timerType} timeLeft={seconds}/>
   </div>
   );
 }
