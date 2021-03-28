becf5db6285ac9c091bb83d149b38af75c7c2223
asorsz <asorsz@gmail.com>
2021-03-28 19:55:41 +0100

    Add beep

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,20 @@
 import '../css/Display.css';
 
 const Display = (props) => {
+  const getDisplay = (secs) => {
+    const mins = Math.floor(secs / 60);
+    const minsPad = mins <= 9 ? `0${mins}` : mins;
+    const rem = secs % 60;
+    const remPad = rem <= 9 ? `0${rem}` : rem;
+    const display = `${minsPad}:${remPad}`;
+    return display;
+  };
+
   return (
     <div className="display">
       <div id="timer-label">{props.label}</div>
-      <h1 id="time-left">{props.timeLeft}</h1>
+      <h1 id="time-left">{getDisplay(props.timeLeft)}</h1>
+      <audio id="beep" src="./audio/beep.wav" preload="auto"></audio>
     </div>
   )
 };
